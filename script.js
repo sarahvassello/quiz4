ece9029207bda662b5ba148f3e8789c7fbb2d600
sarahvassello <sarah.vassello@gmail.com>
2016-02-04 11:27:29 -0500

    setup

---
+++ script.js
@@ -0,0 +1,22 @@
+var students = ["Joe", "Jack", "Jane", "Jill"];
+var gradeMean = [90, 68, 92, 88];
+
+
+for(var=gradeMean; i=0; i<gradeMean; i++;){
+    console.log(students[i]);
+    console.log(students[i] + "has a score of " + gradeMean[i]);
+}
+
+
+if ([students] > 68) {
+    console.log("Jack has the lowest score with 68");
+}
+
+
+
+//Using the data above write a JavaScript that is linked to an html page that determines who has the lowest score.
+//Write the answer in sentence form that includes both the name of the student and the score they have.
+//Write the answer to both the console and the html page using the document write or other command.
+
+
+//jack has the lowest score with 68
